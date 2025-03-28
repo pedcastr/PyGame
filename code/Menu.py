7af09919cb2ce14bf76c07a8280e1da8d9f049a7
pedcastr <pedrohenriquecastro.martins@gmail.com>
2025-03-28 15:09:30 -0300

    Fixado imports no arquivo level.py

--- code/Menu.py
+++ code/Menu.py
@@ -10,7 +10,7 @@ class Menu:
         pygame.mixer.init()
 
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
